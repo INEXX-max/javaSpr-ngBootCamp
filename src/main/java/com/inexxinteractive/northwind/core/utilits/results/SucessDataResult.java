f6c2dc1a25cfa390f5593c0feaa2e29f7b210270
INEXX-max <liosa.yt23@gmail.com>
2026-08-04 22:22:03 +0300

    sss

--- src/main/java/com/inexxinteractive/northwind/core/utilits/results/SucessDataResult.java
+++ src/main/java/com/inexxinteractive/northwind/core/utilits/results/SucessDataResult.java
@@ -1,4 +1,12 @@
 package com.inexxinteractive.northwind.core.utilits.results;
 
-public class SucessDataResult {
+public class SucessDataResult <T> extends DataResults{
+    public SucessDataResult(T data,String message){
+        super (data,true ,message);
+
+    }
+    public SucessDataResult(T data){
+        super (data,true );
+
+    }
 }
