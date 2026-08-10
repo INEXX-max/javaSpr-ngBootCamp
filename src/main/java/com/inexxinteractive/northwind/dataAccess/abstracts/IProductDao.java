aaa10d17903b1d3a3aeff055c6c08fd988fdcac8
INEXX-max <liosa.yt23@gmail.com>
2026-08-10 21:47:32 +0300

    farketmez

--- src/main/java/com/inexxinteractive/northwind/dataAccess/abstracts/IProductDao.java
+++ src/main/java/com/inexxinteractive/northwind/dataAccess/abstracts/IProductDao.java
@@ -21,6 +21,9 @@ public interface IProductDao extends JpaRepository<Product, Integer> {
 
     List<Product> getByProductNameStartingWith(String productName);
 
-    @Query("FROM Product p WHERE p.productName = :productName AND p.Category.categoryId = :categoryId")
+    // YANLIŞ KULLANIM: p.Category.categoryId (Büyük C)
+// DOĞRU KULLANIM: p.category.categoryId (Küçük c)
+
+    @Query("FROM Product p WHERE p.productName = :productName AND p.category.categoryId = :categoryId")
     List<Product> getByNameAndCategory(@Param("productName") String productName, @Param("categoryId") int categoryId);
 }
